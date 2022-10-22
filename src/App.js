f39cd1f1bb8f833de077e6b1f58afb41d720958b
siafromspace <sharonyak15@gmail.com>
2022-10-22 23:54:00 +0100

    layout setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>This is a header</h1>
       </header>
-    </div>
+      <main>
+        <h1>This is the main</h1>
+      </main>
+      <footer>
+        <h1>This is the footer</h1>
+      </footer>
+    </>
   );
 }
 
