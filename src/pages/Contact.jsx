b7a836baea3db4cc99113d9fe08a210d407a93c0
siafromspace <sharonyak15@gmail.com>
2022-11-05 01:55:39 +0100

    feat: implemented form validation feature

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 const Contact = () => {
 
     const name = "Sharon Yakubu"
+
+    const [inputValue, setInputValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value)
+        if (inputValue.length > 0) {
+            setErrorMessage('')
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (inputValue.length <= 0) {
+            setErrorMessage('Please enter a message')
+        } else {
+            alert("Message sent!")
+            setInputValue(e.target.value)
+            window.location.href = "/contact"
+        }
+    }
 
     return (
         <main className="contact__page">
             <div className="contact__container">
                 <h1 className="contact__title">Contact Me</h1>
                 <p className="contact__text">Hi there, contact me to ask me about anything you have in mind.</p>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="name__container">
                         <div className="form__child">
                             <label htmlFor="first_name" className="form__child-title">First Name</label>
@@ -24,10 +47,11 @@ const Contact = () => {
                     </div>
                     <div className="form__child">
                         <label htmlFor="message" className="form__child-title">Message</label>
-                        <textarea name="message" id="message" placeholder="Send me a message and I will reply you as soon as possible..." required></textarea>
+                        <textarea className={`${errorMessage !== '' ? 'error__border' : ''}`} name="message" id="message" placeholder="Send me a message and I will reply you as soon as possible..." onChange={handleChange} value={inputValue}></textarea>
+                        {errorMessage !== '' && <p className="error__msg">{errorMessage}</p>}
                     </div>
                     <div className="checkbox">
-                        <input type="checkbox" name="data" />
+                        <input type="checkbox" name="data" required />
                         <label htmlFor="data">You agree to providing your data to {name} who may contact you.</label>
                     </div>
                     <button id="btn__submit">Send Message</button>
